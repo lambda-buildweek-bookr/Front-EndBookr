bb1e9b351b6d16f64348537862c14b8e8bbbe95d
ills5771 <ilyayelizarov1@gmail.com>
2019-03-12 10:14:30 -0700

    styled Books.js -Ilya Yelizarov

--- bookr/src/components/Books.js
+++ bookr/src/components/Books.js
@@ -5,23 +5,23 @@ import Book from "./Book";
 class Books extends Component {
   render() {
     return (
-      <div className="book-page">
-        <h1>List Of Books</h1>
-        <ul>
-          {this.props.books.map(book => {
-            return (
-              <Book
-                title={book.title}
-                id={book.id}
-                author={book.author}
-                description={book.brief_desc}
-                image={book.image_url}
-                key={book.id}
-                deleteBook={this.props.deleteBook}
-              />
-            );
-          })}
-        </ul>
+      <div className="container">
+        {/* <h1>List Of Books</h1> */}
+        {/* <ul> */}
+        {this.props.books.map(book => {
+          return (
+            <Book
+              title={book.title}
+              id={book.id}
+              author={book.author}
+              description={book.brief_desc}
+              image={book.image_url}
+              key={book.id}
+              deleteBook={this.props.deleteBook}
+            />
+          );
+        })}
+        {/* </ul> */}
       </div>
     );
   }
